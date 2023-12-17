8e2d6671ccc14800f2b87786d7c099cb46f18b4f
Александр Кузьмин <AlexanderKyzmin@yandex.ru>
2023-12-17 20:12:36 +0300

    rewiev # 2(ver # 1)

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -133,15 +133,6 @@ class SubscribeSerializer(ProjectUserSerializer):
         return SmallRecipeSerializer(recipes, many=True).data
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
-
-
 class IngredientSerializer(serializers.ModelSerializer):
     """Сериализатор для модели ингридиенты."""
 
